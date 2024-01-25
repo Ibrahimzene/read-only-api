4c8e47d009becd9d087872b0ae32cdab4da259c9
Ibrahimzene <131875836+Ibrahimzene@users.noreply.github.com>
2024-01-25 13:47:20 +0100

    Add skill import and error handling

--- cli/import.js
+++ cli/import.js
@@ -1,12 +1,17 @@
+import { addSkill } from "../src/handlers.js";
 import * as tools from './tools.js';
+
+const locked = true;
 
 const _skills = tools.readJsonFile('cli/data/skills.json');
 
 for (const _skill of _skills) {
 	try {
-	const skill = await addSkill(_skill);
+		if (locked) {
+		}
+		const skill = await addSkill(_skill);
 	}
 	catch (e) {
-		console.log(``);
+		console.log(`ERROR ${e.message}`);
 	}
 }
